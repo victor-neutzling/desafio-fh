b753d4c2a794537e9afb0cb00d136fd9a8628123
victor neutzling <victorneutzling2001@gmail.com>
2025-11-06 18:11:00 -0300

    fix: ignore dist and test coverage

--- dist/config/database.js
+++
@@ -1,4 +0,0 @@
-import { Pool } from "pg";
-export const pool = new Pool({
-    connectionString: "postgresql://root:root@db:5432/fhchallenge",
-});
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./graphql/schema/usersSchema.js";
-import { resolvers } from "./graphql/resolvers/userResolvers.js";
-const server = new ApolloServer({ typeDefs, resolvers });
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-});
-console.log(`server running at: ${url}`);
