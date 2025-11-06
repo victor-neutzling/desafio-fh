ae481f37aa22599308f0dcf77891dc2a1db13cb0
victor neutzling <victorneutzling2001@gmail.com>
2025-11-06 18:10:09 -0300

    fix: ignore dist and test coverage

--- dist/config/database.d.ts
+++
@@ -1,3 +0,0 @@
-import { Pool } from "pg";
-export declare const pool: Pool;
-//# sourceMappingURL=database.d.ts.map
--- dist/interfaces/user.d.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    id: string;
-    name: string;
-    balance: number;
-}
-//# sourceMappingURL=user.d.ts.map
